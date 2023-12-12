1b025c06e1605fb7bb373920167bc80b8f685d96
mjbarahonaga <66804505+mjbarahonaga@users.noreply.github.com>
2023-12-12 18:37:56 +0100

    Improve feedback

--- Assets/Scripts/ClickFeedback.cs
+++ Assets/Scripts/ClickFeedback.cs
@@ -21,20 +21,20 @@ public class ClickFeedback : MonoBehaviour
         ValueClick.text = "+" + amount.ToString();
         CanvasGroup.alpha = 0.5f;
         _startTime = Time.time;
-        _updateCoroutine = Timing.RunCoroutine(MyUpdate());
+        _updateCoroutine = Timing.RunCoroutine(MyUpdate(), Segment.Update);
 
     }
 
     public IEnumerator<float> MyUpdate()
     {
         float currentTime = 0f;
-        float valueX = UnityEngine.Random.Range(-3f, 3f);
-        float valueY = UnityEngine.Random.Range(-3f, 4f);
+        float valueX = UnityEngine.Random.Range(-300f, 300f);
+        float valueY = UnityEngine.Random.Range(-100f, 100f);
         float speedY = UnityEngine.Random.Range(1f, 3f);
 
-        //Vector3 addPos = new Vector3(valueX, valueY, 0f);
+        Vector3 addPos = new Vector3(valueX, valueY, 0f);
         Vector3 upSpeed = new Vector3(0f, speedY, 0f);
-        //transform.position += addPos;
+        transform.position += addPos;
         while (true)
         {
             currentTime = Time.time - _startTime;
